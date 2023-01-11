650f144f25d74295f28ebfd861f17e41c2829044
31Souf <saifisophiane@gmail.com>
2023-01-11 14:23:17 +0100

    shellcode-c created

---
+++ shellcodes/shellcode01.s
@@ -0,0 +1,30 @@
+	.file	"shellcode01.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"/bin/sh"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB6:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, %edx
+	movl	$0, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	call	execve@PLT
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	main, .-main
+	.ident	"GCC: (Debian 12.2.0-9) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
