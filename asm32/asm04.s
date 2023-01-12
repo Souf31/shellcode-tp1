186c5c862e7cd018e7aa0c72253bb524a0122f61
31Souf <saifisophiane@gmail.com>
2023-01-12 16:56:28 +0100

    asm04 works in x86_32

---
+++ asm32/asm04.s
@@ -0,0 +1,46 @@
+section .data                           ;Data segment
+   userMsg db 'Please enter a number: ' ;Ask the user to enter a number
+   lenUserMsg equ $-userMsg             ;The length of the message
+   number:     db '1337', 4
+ 	 numberLen:  equ $-number
+
+section .bss           ;Uninitialized data
+  num resb 5
+
+section .text          ;Code Segment
+  global _start
+
+_start:                ;User prompt
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, userMsg
+  mov edx, lenUserMsg
+  int 80h
+
+  ;Read and store the user input
+  mov eax, 3
+  mov ebx, 2
+  mov ecx, num
+  mov edx, 5          ;5 bytes (numeric, 1 for sign) of that information
+  int 80h
+
+  mov al, byte[num]
+  and al, 1
+  jz end_return_1_even
+
+  ;Output the message '1337'
+  mov eax, 4
+  mov ebx, 1
+  mov ecx, number
+  mov edx, numberLen
+  int 80h
+
+  ; Exit code
+  mov eax, 1
+  mov ebx, 0
+  int 80h
+
+end_return_1_even:
+  mov eax, 1
+  mov ebx, 1
+  int 80h
